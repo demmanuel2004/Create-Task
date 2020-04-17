c84a444974a573513e2b0014016c65019894c3cc
LukeM <lmikulenas@ucvts.org>
2020-04-17 13:30:30 -0400

    Added month addition, day to come soon (Lukas)

--- Main.py
+++ Main.py
@@ -1,6 +1,6 @@
 from datetime import datetime, date
 
-JANUARY = 31
+"""JANUARY = 31
 FEBRUARY = 28
 MARCH = 31
 APRIL = 30
@@ -11,11 +11,55 @@ AUGUST = 31
 SEPTEMBER = 30
 OCTOBER = 31
 NOVEMBER = 30
-DECEMBER = 31 
+DECEMBER = 31""" 
+
+"""def day_year(user_year):
+  if user_year <= 1699 and user_year >= 1600:
+     global a 
+     a = 6 
+  print (a)
+
+  elif user_year <= 1799 and user_year >= 1700:
+      global year_addition_18th
+      year_addition_18th = 4 
+  print (year_addition_18th)"""
+
+def day_month(user_month):
+  if user_month == 1 or user_month == 10:
+    global month_addition
+    month_addition = 0 
+    print (month_addition)
+  elif user_month == 2 or user_month == 3:
+    month_addition = 3 
+    print (month_addition)
+  elif user_month == 4 or user_month == 7:
+    month_addition = 6
+    print (month_addition)
+  elif user_month == 5:
+    month_addition = 1
+    print (month_addition)
+  elif user_month == 6:
+    month_addition = 4
+    print (month_addition)
+  elif user_month == 7:
+    month_addition = 6
+    print (month_addition)
+  elif user_month == 8:
+    month_addition = 2
+    print (month_addition)
+  elif user_month == 9 or user_month == 12:
+    month_addition = 5
+    print (month_addition)
+  elif user_month == 11:
+    month_addition = 3
+    print (month_addition)
+
 
 name = input("Welcome user, what is your name? \n"); 
 
 user_year = int(input("Welcome " + name +" \n" "Enter the year between the years 1600 and 2099. Please format it in numbers only <1999, etc>: \n"));
+
+"day(user_year)"
 
 if user_year % 4 == 0:
   FEBRUARY = 29
@@ -23,17 +67,17 @@ if user_year % 4 == 0:
 while user_year > 3000 or user_year < 1599:
  user_year = int(input("Please enter a year that is between 1600 and 2099 \n"))
 
-user_month = input("Now " + name + " Enter the month your day is located in. Please format it in words only <June, etc.> \n")
+user_month = int(input("Now " + name + " Enter the number of the month your day is located in. Please format it in numbers only <for June it will be 6> \n"))
+day_month(user_month)
 
-user_month_fixed = user_month.upper()
+while user_month > 13 or user_year < 0:
+ user_month = int(input("Please enter a month that is between 1 and 12\n"))
 
-print (user_month_fixed)
 
 user_day = int(input("Finally " + name + " Enter the number day of the month in numbers please <1, 3, etc> \n"))
 
 while user_day > 31 or user_year < 0:
  user_day = int(input("Please enter a day that is between 31 and 1 \n"))
-
 
 
 # To find out the no of days between two given dates
